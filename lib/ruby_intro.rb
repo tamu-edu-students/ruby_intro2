88b026ea439aa39814542347a605808c7ff910b6
Tianchen Huang <tianchenhuang@Tianchens-MacBook-Pro.local>
2024-02-07 02:35:08 -0600

    try alternatives

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,11 +7,29 @@ def sum(arr)
   arr.reduce(0, :+)
 end
 
+# alternative
+def sum2(arr)
+  arr.inject(0) { |sum, num| sum + num }
+end
+
+
 def max_2_sum(arr)
   # YOUR CODE HERE
   return 0 if arr.empty?      
   return arr.first if arr.length == 1
   arr.max(2).reduce(:+)
+end
+
+# alternative
+def max_2_sum2(arr)
+  case arr.size
+  when 0
+    0
+  when 1
+    arr.first
+  else
+    arr.sort.last(2).sum
+  end
 end
 
 def sum_to_n?(arr, number)
@@ -20,6 +38,18 @@ def sum_to_n?(arr, number)
   return false if arr.length == 1
   arr.combination(2).any? { |a, b| a + b == number }
 end
+
+# alternative
+def sum_to_n?(arr, number)
+  return false if arr.length < 2
+  complements = {}
+  arr.each do |element|
+    return true if complements[element]
+    complements[number - element] = true
+  end
+  false
+end
+
 
 # Part 2
 
